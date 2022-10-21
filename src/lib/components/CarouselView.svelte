eacf8de5f08fc392f2197d7030c844c5179c9521
Magnus Karström <97023772+yeswebdotse@users.noreply.github.com>
2022-10-21 22:40:54 +0200

    fix (CarouselView.svelte): Found bug -  not working atm

--- src/lib/components/CarouselView.svelte
+++ src/lib/components/CarouselView.svelte
@@ -1,39 +1,39 @@
-<div class="carousel carousel-center max-w-full p-4 space-x-4 bg-neutral rounded-box">
+<section class="carousel carousel-center max-w-[400px] p-4 space-x-4 bg-neutral ">
 	<div class="carousel-item">
-		<img src="../lib/img/Men.jpg" class="rounded-box w-48" />
+		<img src="/src/lib/img/Men.jpg" class="rounded-box w-48" />
 	</div>
 	<div class="carousel-item">
-		<img src="../lib/img/Men.jpg" class="rounded-box w-48" />
+		<img src="/src/lib/img/Men.jpg" class="rounded-box w-48" />
 	</div>
 	<div class="carousel-item">
-		<img src="../lib/img/Men.jpg" class="rounded-box w-48" />
-	</div>
-
-	<div class="carousel-item">
-		<img src="../lib/img/Men.jpg" class="rounded-box  w-48" />
+		<img src="/src/lib/img/Men.jpg" class="rounded-box w-48" />
 	</div>
 
 	<div class="carousel-item">
-		<img src="../lib/img/Men.jpg" class="rounded-box  w-48" />
+		<img src="/src/lib/img/Men.jpg" class="rounded-box  w-48" />
 	</div>
 
 	<div class="carousel-item">
-		<img src="../lib/img/Men.jpg" class="rounded-box  w-48" />
+		<img src="/src/lib/img/Men.jpg" class="rounded-box  w-48" />
 	</div>
 
 	<div class="carousel-item">
-		<img src="../lib/img/Men.jpg" class="rounded-box  w-48" />
+		<img src="/src/lib/img/Men.jpg" class="rounded-box  w-48" />
 	</div>
 
 	<div class="carousel-item">
-		<img src="../lib/img/Men.jpg" class="rounded-box  w-48" />
+		<img src="/src/lib/img/Men.jpg" class="rounded-box  w-48" />
 	</div>
 
 	<div class="carousel-item">
-		<img src="src\lib\img\Men.jpg" class="rounded-box  w-48" />
+		<img src="/src/lib/img/Men.jpg" class="rounded-box  w-48" />
 	</div>
 
 	<div class="carousel-item">
-		<img src="src\lib\img\Men.jpg" class="rounded-box  w-48" />
+		<img src="/src/lib/img/Men.jpg" class="rounded-box  w-48" />
 	</div>
-</div>
+
+	<div class="carousel-item">
+		<img src="/src/lib/img/Men.jpg" class="rounded-box  w-48" />
+	</div>
+</section>
