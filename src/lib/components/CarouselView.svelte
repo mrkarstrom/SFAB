cf12065267c74434dc565649806f415336ca1363
Magnus Karström <magnus@yesweb.se>
2022-10-21 14:51:57 +0200

    fix (CarouselView.svelte): Added img and routes

--- src/lib/components/CarouselView.svelte
+++ src/lib/components/CarouselView.svelte
@@ -1,12 +1,32 @@
 <div class="carousel carousel-center max-w-full p-4 space-x-4 bg-neutral rounded-box">
 	<div class="carousel-item">
-		<img src="src\lib\img\Men.jpg" class="rounded-box w-48" />
+		<img src="../lib/img/Men.jpg" class="rounded-box w-48" />
 	</div>
 	<div class="carousel-item">
-		<img src="src\lib\img\Men.jpg" class="rounded-box w-48" />
+		<img src="../lib/img/Men.jpg" class="rounded-box w-48" />
 	</div>
 	<div class="carousel-item">
-		<img src="src\lib\img\Men.jpg" class="rounded-box w-48" />
+		<img src="../lib/img/Men.jpg" class="rounded-box w-48" />
+	</div>
+
+	<div class="carousel-item">
+		<img src="../lib/img/Men.jpg" class="rounded-box  w-48" />
+	</div>
+
+	<div class="carousel-item">
+		<img src="../lib/img/Men.jpg" class="rounded-box  w-48" />
+	</div>
+
+	<div class="carousel-item">
+		<img src="../lib/img/Men.jpg" class="rounded-box  w-48" />
+	</div>
+
+	<div class="carousel-item">
+		<img src="../lib/img/Men.jpg" class="rounded-box  w-48" />
+	</div>
+
+	<div class="carousel-item">
+		<img src="../lib/img/Men.jpg" class="rounded-box  w-48" />
 	</div>
 
 	<div class="carousel-item">
@@ -16,25 +36,4 @@
 	<div class="carousel-item">
 		<img src="src\lib\img\Men.jpg" class="rounded-box  w-48" />
 	</div>
-
-	<div class="carousel-item">
-		<img src="src\lib\img\Men.jpg" class="rounded-box  w-48" />
-	</div>
-
-	<div class="carousel-item">
-		<img src="src\lib\img\Men.jpg" class="rounded-box  w-48" />
-	</div>
-	
-	<div class="carousel-item">
-		<img src="src\lib\img\Men.jpg" class="rounded-box  w-48" />
-	</div>
-	
-	<div class="carousel-item">
-		<img src="src\lib\img\Men.jpg" class="rounded-box  w-48" />
-	</div>
-	
-	<div class="carousel-item">
-		<img src="src\lib\img\Men.jpg" class="rounded-box  w-48" />
-	</div>
-	
 </div>
