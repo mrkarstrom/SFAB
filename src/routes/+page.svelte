826d884857685ff7daa8e0d4ad3004e4a00c0ac9
Magnus Karström <97023772+yeswebdotse@users.noreply.github.com>
2022-10-21 22:26:09 +0200

    fix (logo, Men, Women): deleted from root

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <script>
 	import HeroSplit from '$lib/components/HeroSplit.svelte';
 	import CarouselView from '$lib/components/CarouselView.svelte';
-	import Hero from '$lib/components/Hero.svelte';
 	import CarouselBanner from '$lib/components/CarouselBanner.svelte';
 </script>
 
