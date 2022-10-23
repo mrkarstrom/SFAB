a25f5d51a89dc99b5fb9f1bacd243ddf7e3a01ea
Magnus Karström <97023772+yeswebdotse@users.noreply.github.com>
2022-10-23 02:42:28 +0200

    testin a source

--- src/lib/components/CarouselView.svelte
+++ src/lib/components/CarouselView.svelte
@@ -1,7 +1,7 @@
 <!-- <div class="wrapper w-full bg-transparent flex flex-row"> -->
 <section class="carousel carousel-center max-w-full p-4 space-x-4 bg-neutral ">
 	<div class="carousel-item">
-		<img src="https://placeimg.com/1000/800/arch" class="rounded-box w-48" />
+		<img src="https://github.com/mrkarstrom/SFAB/blob/main/src/lib/img/men.jpg" class="rounded-box w-48" />
 	</div>
 	<div class="carousel-item">
 		<img src="https://placeimg.com/1000/800/arch" class="rounded-box w-48" />
