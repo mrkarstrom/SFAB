605896d48c990ba2be3b6cac037a1f88d366eab6
Magnus Karström <magnus@yesweb.se>
2022-10-25 13:28:23 +0200

    feat (CarouselView.svelte): insering items from itemsStore.js

--- src/lib/components/CarouselView.svelte
+++ src/lib/components/CarouselView.svelte
@@ -5,16 +5,17 @@
 <section class="carousel carousel-center max-w-full p-4 space-x-4 bg-neutral ">
 	{#each $itemsStore as item}
 		<div class="carousel-item">
-			<div class="card w-96 bg-base-100 shadow-xl">
-				<figure>
-					<img src={item.link} class="rounded-box w-[90%] pt-2" alt={item.label} />
+			<div class="card rounded-sm w-80 bg-base-100 shadow-xl">
+				<figure class="object-cover object-center">
+					<img src={item.link} class="none-rounded h-96 pt-2 " alt={item.label} />
 				</figure>
 				<div class="card-body">
 					<h2 class="card-title">
-						{item.gender}
-						<div class="badge badge-secondary">Price: {item.price}</div>
+						Category: {item.gender}
 					</h2>
 					<p>Color: {item.color}</p>
+					<div class="badge badge-secondary">Price: {item.price}</div>
+
 					<div class="card-actions justify-end">
 						<div class="badge badge-outline">Price: {item.price}</div>
 						<div class="badge badge-outline">Label: {item.label}</div>
@@ -23,37 +24,5 @@
 			</div>
 		</div>
 	{/each}
-
-	<div class="carousel-item">
-		<img src="https://placeimg.com/1000/800/arch" class="rounded-box w-48" />
-	</div>
-
-	<div class="carousel-item">
-		<img src="https://placeimg.com/1000/800/arch" class="rounded-box  w-48" />
-	</div>
-
-	<div class="carousel-item">
-		<img src="https://placeimg.com/1000/800/arch" class="rounded-box  w-48" />
-	</div>
-
-	<div class="carousel-item">
-		<img src="https://placeimg.com/1000/800/arch" class="rounded-box  w-48" />
-	</div>
-
-	<div class="carousel-item">
-		<img src="https://placeimg.com/1000/800/arch" class="rounded-box  w-48" />
-	</div>
-
-	<div class="carousel-item">
-		<img src="https://placeimg.com/1000/800/arch" class="rounded-box  w-48" />
-	</div>
-
-	<div class="carousel-item">
-		<img src="https://placeimg.com/1000/800/arch" class="rounded-box  w-48" />
-	</div>
-
-	<div class="carousel-item">
-		<img src="https://placeimg.com/1000/800/arch" class="rounded-box  w-48" />
-	</div>
 </section>
 <!-- </div> -->
--- src/stores/itemsStore.js
+++ src/stores/itemsStore.js
@@ -2,10 +2,27 @@ import { writable } from 'svelte/store';
 
 const items = writable([
 	{
+		gender: 'Women',
+		label: 'Suit',
+		price: '€5500',
+		color: 'Black',
+		brand: 'Gucci',
+		link: 'https://yesweb.se/sfab/img/women/suits/suit-black.jpg'
+	},
+	{
+		gender: 'Women',
+		label: 'Dress',
+		price: '€8000',
+		color: 'Red',
+		brand: 'Prada',
+		link: 'https://yesweb.se/sfab/img/women/dresses/dress-red.jpg'
+	},
+	{
 		gender: 'Men',
 		label: 'Suit',
 		price: '€5400',
 		color: 'Dark Blue',
+		brand: 'Gucci',
 		link: 'https://yesweb.se/sfab/img/men/suits/suit-darkblue.jpg'
 	},
 	{
@@ -13,7 +30,8 @@ const items = writable([
 		label: 'Pants',
 		price: '€540',
 		color: 'Blue',
-		link: 'https://yesweb.se/sfab/img/men/suits/suit-darkblue.jpg'
+		brand: 'Prada',
+		link: 'https://yesweb.se/sfab/img/men/pants/pants-blue.jpg'
 	}
 ]);
 
