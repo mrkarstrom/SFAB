97a7ef89be43041fa9077b69625aec1eaa42e996
Magnus Karström <magnus@yesweb.se>
2022-10-25 15:27:52 +0200

    fix (HeaderSplit.svelte): Getting the correct image-sizes and cover

--- src/lib/components/HeroSplit.svelte
+++ src/lib/components/HeroSplit.svelte
@@ -1,16 +1,16 @@
 <div class="flex h-screen w-full relative overflow-y-hidden z-10">
-	<div class="w-[50%] max-h-80">
+	<div class="w-[50%] h-full">
 		<img
 			src="https://yesweb.se/sfab/img/women/shirts/blouse-white.jpg"
-			class="object-contain object-center"
+			class="object-cover object-center min-h-full"
 			alt=""
 		/>
 		<button class="btn btn-neutral top-[50%] absolute left-3">Shop For Her</button>
 	</div>
-	<div class="w-[50%] max-h-80">
+	<div class="w-[50%] h-full">
 		<button class="btn btn-neutral  top-[50%] absolute right-3">Shop For Him</button>
 		<img
-			class="object-center object-fill"
+			class="object-center object-cover min-h-full"
 			src="https://yesweb.se/sfab/img/men/suits/suit-darkblue.jpg"
 			alt=""
 		/>
