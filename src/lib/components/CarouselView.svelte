410f9c1a7f9f23604ea18d3409eb28221c8b67e6
Magnus Karström <97023772+yeswebdotse@users.noreply.github.com>
2022-10-27 01:33:44 +0200

    fix (CarouselView): Correcting image sizing in card figure

--- src/lib/components/CarouselView.svelte
+++ src/lib/components/CarouselView.svelte
@@ -5,12 +5,11 @@
 <section class="carousel carousel-center max-w-full p-4 space-x-4 bg-neutral ">
 	{#each $itemsStore as item}
 		<div class="carousel-item ">
-			<div class="card rounded-sm w-80  bg-base-100 shadow-xl ">
-				<figure class="h-[60%] overflow-hidden">
-					<!-- todo -->
+			<div class="card rounded-sm w-80 bg-base-100 shadow-xl ">
+				<figure class="h-[60%] mt-2 overflow-hidden">
 					<img
 						src={item.link}
-						class="none-rounded p-2 object-cover object-center "
+						class="px-2 none-rounded object-cover object-center "
 						alt={item.label}
 					/>
 				</figure>
