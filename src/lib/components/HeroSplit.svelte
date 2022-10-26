bf91631cbabcb69eeb833f06e7083be9a9abff68
Magnus Karström <97023772+yeswebdotse@users.noreply.github.com>
2022-10-26 04:36:58 +0200

    fix (HeroSplit.svelte): buttons rounded-none

--- src/lib/components/HeroSplit.svelte
+++ src/lib/components/HeroSplit.svelte
@@ -5,10 +5,10 @@
 			class="object-cover object-center min-h-full"
 			alt=""
 		/>
-		<button class="btn btn-neutral top-[50%] absolute left-3">Shop For Her</button>
+		<button class="btn btn-neutral rounded-none top-[50%] absolute left-3">Shop For Her</button>
 	</div>
 	<div class="w-[50%] h-full">
-		<button class="btn btn-neutral  top-[50%] absolute right-3">Shop For Him</button>
+		<button class="btn btn-neutral rounded-none top-[50%] absolute right-3">Shop For Him</button>
 		<img
 			class="object-center object-cover min-h-full"
 			src="https://yesweb.se/sfab/img/men/suits/suit-darkblue.jpg"
