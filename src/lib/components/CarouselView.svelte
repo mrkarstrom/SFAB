85f3db5d1f9866999acb8b0b9b4adabe37337601
Magnus Karström <97023772+yeswebdotse@users.noreply.github.com>
2022-10-27 02:02:24 +0200

    fix (CarouselView): Correcting image sizing in card figure

--- src/lib/components/CarouselView.svelte
+++ src/lib/components/CarouselView.svelte
@@ -2,11 +2,11 @@
 	import itemsStore from '../../stores/itemsStore';
 </script>
 
-<section class="carousel carousel-center max-w-full p-4 space-x-4 bg-neutral ">
+<section class="carousel carousel-center max-h-[60%] max-w-full p-4 space-x-4 bg-neutral ">
 	{#each $itemsStore as item}
-		<div class="carousel-item ">
-			<div class="card rounded-sm w-80 bg-base-100 shadow-xl ">
-				<figure class="h-[60%] mt-2 overflow-hidden">
+		<div class="carousel-item lg:h-[500px] lg:w-[300px] h-96">
+			<div class="card rounded-sm max-w-full bg-base-100 shadow-xl ">
+				<figure class="mt-2 overflow-hidden">
 					<img
 						src={item.link}
 						class="px-2 none-rounded object-cover object-center "
@@ -15,15 +15,11 @@
 				</figure>
 				<div class="card-body">
 					<h2 class="card-title">
-						Category: {item.gender}
+						Category: {item.label} - {item.gender}
 					</h2>
 					<p>Color: {item.color}</p>
-					<div class="badge badge-secondary">Price: {item.price}</div>
-
-					<div class="card-actions justify-end">
-						<div class="badge badge-outline">Price: {item.price}</div>
-						<div class="badge badge-outline">Label: {item.label}</div>
-					</div>
+					<div class="badge badge-secondary">Add item</div>
+					<div class="badge badge-outline">Price: {item.price}</div>
 				</div>
 			</div>
 		</div>
