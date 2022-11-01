d9d8bbb3728123123eb9eea210cf647efdf04b29
Magnus Karström <magnus@yesweb.se>
2022-11-01 15:40:23 +0100

    feat (HeroVideo.svelte): Autoplay enabled, TODO->hide controls

--- src/lib/components/HeroVideo.svelte
+++ src/lib/components/HeroVideo.svelte
@@ -1,15 +1,7 @@
 <script>
 	// export const video = 'https://yesweb.se/sfab/video/SlowFashionVideo.mp4';
-	export const video = '..src/lib/video/SlowFashionVideo.mp4';
+	const video = '/src/lib/video/SlowFashionVideo.mp4';
 </script>
 
-<video
-	src={video}
-	loop
-	controls="false"
-	autoplay="autoplay"
-	type="video/mp4"
-	class="w-full"
-	alt="Slowfashion Video"
-	style="pointer-events:none"
-/>
+<video src={video} class="w-full" loop autoplay="autoplay"  controls="false" />
+<!-- <iframe src=`{video}+"?autoplay=1&controls=0&&showinfo=0&loop=1"` class="w-full" /> -->
