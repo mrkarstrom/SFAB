b9c4ccb4dc6603322bc99d746afef6f2d99e06f4
Magnus Karström <magnus@yesweb.se>
2022-11-07 14:17:21 +0100

    feat (Footer.svelte): Footer is looking good!

--- src/lib/components/footer/Footer.svelte
+++ src/lib/components/footer/Footer.svelte
@@ -1,9 +1,28 @@
 <script>
 </script>
 
-<div class="flex flex-col items-center justify-cneter w-full h-[200px] bg-white" />
 <div
-	class="footer-1-border flex flex-col items-center justify-center w-full b-0  h-[50px] uppercase bg-[#ecc692] text-gray-500"
+	class="flex flex-col items-center justify-center w-full h-[70px] border-t-2 border-gray-400 bg-white"
+>
+	<ul
+		class="flex flex-row items-center font-normal  text-gray-500 w-full justify-center space-x-7 "
+	>
+		<li>
+			<a href="#" class="uppercase hover:text-gray-900">About us</a>
+		</li>
+		<li>
+			<a href="#" class="uppercase hover:text-gray-900">Contact us</a>
+		</li>
+		<li>
+			<a href="#" class="uppercase hover:text-gray-900">Sustainability</a>
+		</li>
+		<li>
+			<a href="#" class="uppercase hover:text-gray-900">App</a>
+		</li>
+	</ul>
+</div>
+<div
+	class="footer-1-border flex flex-col items-center justify-center w-full b-0  h-[35px] uppercase bg-[#edc692] text-gray-800"
 >
 	&copy;Slowfashion 2022
 </div>
