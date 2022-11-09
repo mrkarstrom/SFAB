f527b94bb4fa4e2e82305f9e6756c354b7b96c07
Magnus Karström <magnus@yesweb.se>
2022-11-09 09:57:31 +0100

    fix (LinkbarHero.svelte): Setting the circles correctly on the hero

--- src/lib/components/linkbar/LinkbarHero.svelte
+++ src/lib/components/linkbar/LinkbarHero.svelte
@@ -1,10 +1,12 @@
 <div class="absolute left-8">
-	<div class="some-wrapper flex flex-row space-y-4 md:top-10 md:h-64 md:w-10">
-		<ul>
-			<li class="w-8 h-8 border-2 border-white rounded-full" />
-			<li class="w-8 h-8 border-2 border-white rounded-full" />
-			<li class="w-8 h-8 border-2 border-white rounded-full" />
-			<li class="w-8 h-8 border-2 border-white rounded-full" />
+	<div
+		class="some-wrapper hidden md:flex flex-row items-center justify-center mt-16 space-y-12 h-64 w-10"
+	>
+		<ul class="space-y-5">
+			<li class="w-10 h-10 border border-white rounded-full" />
+			<li class="w-10 h-10 border border-white rounded-full" />
+			<li class="w-10 h-10 border border-white rounded-full" />
+			<li class="w-10 h-10 border border-white rounded-full" />
 		</ul>
 	</div>
 </div>
--- src/lib/components/topbar/TopbarPlain.svelte
+++ src/lib/components/topbar/TopbarPlain.svelte
@@ -2,7 +2,9 @@
 	export let currentLang = 'EN';
 </script>
 
-<div class="w-screen h-[40px] bg-[white] flex flex-col items-center justify-center">
-	<p class="font-normal text-sm">We Are The Best Marketplace for Pre-Owned High Quality Fashion</p>
+<div class="flex flex-col w-screen h-[40px] bg-[white]  items-center justify-center">
+	<p class="hidden md:flex font-normal text-sm">
+		We Are The Best Marketplace for Pre-Owned High Quality Fashion
+	</p>
 	<button class="absolute right-8">{currentLang}</button>
 </div>
