0bd16e8a272d35abdec0d1304d4fa9cb7b93dfe6
Magnus Karström <magnus@yesweb.se>
2022-11-09 09:09:15 +0100

    feat (LinkbarHero.svelte): Working on the linkbar

--- src/lib/components/hero/Hero.svelte
+++ src/lib/components/hero/Hero.svelte
@@ -1,5 +1,12 @@
+<script>
+	import LinkbarHero from '$lib/components/linkbar/LinkbarHero.svelte';
+</script>
+
 <div class="hero min-h-screen" style="background-image: url(https://placeimg.com/1000/800/arch);">
 	<div class="hero-overlay bg-opacity-60" />
+	<div class="h-full w-content border-red-700">
+		<LinkbarHero />
+	</div>
 	<div class="hero-content text-center text-neutral-content">
 		<div class="max-w-md">
 			<h1 class="mb-5 text-5xl font-bold">Hello there</h1>
---
+++ src/lib/components/linkbar/LinkbarHero.svelte
@@ -0,0 +1,10 @@
+<div class="absolute left-8">
+	<div class="some-wrapper flex flex-row space-y-4 md:top-10 md:h-64 md:w-10">
+		<ul>
+			<li class="w-8 h-8 border-2 border-white rounded-full" />
+			<li class="w-8 h-8 border-2 border-white rounded-full" />
+			<li class="w-8 h-8 border-2 border-white rounded-full" />
+			<li class="w-8 h-8 border-2 border-white rounded-full" />
+		</ul>
+	</div>
+</div>
