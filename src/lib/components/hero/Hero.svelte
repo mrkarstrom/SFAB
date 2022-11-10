4afe5c3b3b0cc8f49959fd53d7b92b9fa1c4ef55
Magnus Karström <magnus@yesweb.se>
2022-11-10 11:51:23 +0100

    fix (Header.svelte): Changing from background image to gradient for mockup

--- src/lib/components/hero/Hero.svelte
+++ src/lib/components/hero/Hero.svelte
@@ -1,8 +1,9 @@
 <script>
 	import LinkbarHero from '$lib/components/linkbar/LinkbarHero.svelte';
+	import ArrowDown from '$lib/components/pointers/ArrowDown.svelte';
 </script>
 
-<div class="hero min-h-screen" style="background-image: url(https://placeimg.com/1000/800/arch);">
+<div class="hero min-h-screen bg-gradient-to-r from-secondary via-[#5a554682] to-white">
 	<div class="hero-overlay bg-opacity-0" />
 	<div class="h-full w-content border-red-700">
 		<LinkbarHero />
@@ -11,16 +12,7 @@
 		<div class="max-w-md flex flex-col justify-center items-center">
 			<p class="mb-3 text-xl text-white uppercase">Be the change</p>
 			<h1 class="mb-16 text-7xl text-white font-['Steelfish'] uppercase ">Slowfashion</h1>
-			<div
-				class="circle-arrow flex justify-center items-center border-['var(--hero-lines)'] border w-16 h-16 rounded-full "
-			>
-				<svg xmlns="http://www.w3.org/2000/svg" width="1rem" fill="white" viewBox="0 0 384 512"
-					><!--! Font Awesome Free 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2022 Fonticons, Inc. -->
-					<path
-						d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"
-					/></svg
-				>
-			</div>
+			<ArrowDown />
 		</div>
 	</div>
 </div>
