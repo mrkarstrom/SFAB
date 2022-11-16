e43051d7934248c16a2562225149cd4dc97c5260
Magnus Karström <magnus@yesweb.se>
2022-11-16 16:18:31 +0100

    feat (ArrowDown.svelte): Variable colors for circle and arrow

--- src/lib/components/hero/Hero.svelte
+++ src/lib/components/hero/Hero.svelte
@@ -5,13 +5,14 @@
 </script>
 
 <div
-	class="backdrop bg-no-repeat bg-center m-0 p-0 h-[800px] w-auto overflow-x-hidden" style="background-image: url('https://yesweb.se/sfab/img/mockup/hero-couple.png') "
+	class="backdrop bg-no-repeat bg-center m-0 p-0 h-[800px] w-auto overflow-x-hidden"
+	style="background-image: url('https://yesweb.se/sfab/img/mockup/hero-couple.png') "
 >
 	<LinkbarHero />
 	<div class="box flex flex-col pt-56 opacity-100 items-center justify-center">
 		<p class="mb-3 text-xl text-white uppercase">Be the change</p>
 		<h1 class="mb-16 text-7xl text-white font-['Steelfish'] uppercase ">Slowfashion</h1>
-		<ArrowDown />
+		<ArrowDown --arrowColor="white" --circleColor="white" />
 	</div>
 </div>
 
