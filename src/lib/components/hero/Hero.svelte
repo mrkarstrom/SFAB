8a83de2a2d893f8202daaf8ffb3f125ea0dcbf1d
Magnus Karström <magnus@yesweb.se>
2022-11-16 16:10:21 +0100

    feat (Hero.svelte): Added the mockup hero-image

--- src/lib/components/hero/Hero.svelte
+++ src/lib/components/hero/Hero.svelte
@@ -5,10 +5,10 @@
 </script>
 
 <div
-	class="backdrop m-0 p-0 h-[800px] w-auto overflow-x-hidden bg-gradient-to-r from-secondary via-[#5a554682] to-white"
+	class="backdrop bg-no-repeat bg-center m-0 p-0 h-[800px] w-auto overflow-x-hidden" style="background-image: url('https://yesweb.se/sfab/img/mockup/hero-couple.png') "
 >
 	<LinkbarHero />
-	<div class="box flex flex-col pt-60 items-center justify-center">
+	<div class="box flex flex-col pt-56 opacity-100 items-center justify-center">
 		<p class="mb-3 text-xl text-white uppercase">Be the change</p>
 		<h1 class="mb-16 text-7xl text-white font-['Steelfish'] uppercase ">Slowfashion</h1>
 		<ArrowDown />
