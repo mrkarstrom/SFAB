745c54b9386fae381ad4dca683befb3ede098bfa
Magnus Karström <magnus@yesweb.se>
2022-11-21 15:33:42 +0100

    fix (+page.svelte): Removed old components and added some new ones

---
+++ src/lib/components/appwindow/AppWindow.svelte
@@ -0,0 +1,18 @@
+<div class="w-full h-screen bg-secondary flex flex-col items-between justify-left">
+	<h2>A Window into the App</h2>
+	<p class="max-w-xl text-center">
+		Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit, voluptatum. Odio laborum
+		facilis accusamus distinctio impedit dicta porro, ipsum voluptatem excepturi tempora
+		consequuntur expedita laboriosam dolores illo dolore similique veritatis? Rerum accusantium
+		animi a quas eius maiores at, quae sit recusandae ratione provident culpa officia soluta.
+		Consectetur, doloremque. Sed dolorem, dolore eligendi temporibus qui reprehenderit explicabo
+		omnis quas corporis suscipit impedit atque quibusdam veniam aliquid et perspiciatis, ipsa
+		dolores fuga ratione laborum maiores reiciendis deleniti! Laboriosam expedita laudantium
+		necessitatibus numquam officiis quod eum, ab voluptatum temporibus accusantium minus! Commodi
+		explicabo iste eum asperiores saepe, quas fugit, laudantium alias velit nostrum praesentium
+		aliquam quod, unde qui. Repudiandae veniam quia velit, unde voluptates ducimus tempora iure
+		voluptate esse, rem odit. Cum doloremque corrupti non neque. Quibusdam nam in nisi soluta sunt
+		obcaecati quis! Ducimus laborum ullam veniam exercitationem tempore, id minus corporis, itaque
+		sed.
+	</p>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
 	import DownloadAppPromo1 from '$lib/components/boards/DownloadAppPromo1.svelte';
 	import Spacer from '$lib/components/spacers/Spacer.svelte';
 	import ArrowBlock from '$lib/components/pointers/ArrowBlock.svelte';
-	import CarouselView from '$lib/components/carousels/CarouselView.svelte';
+	import AppWindow from '$lib/components/appwindow/AppWindow.svelte';
 	import CarouselBanner from '$lib/components/carousels/CarouselBanner.svelte';
 	import Mission1 from '$lib/components/mission/Mission1.svelte';
 </script>
@@ -11,8 +11,8 @@
 <Hero />
 <DownloadAppPromo1 --spacer-height="15rem" />
 <ArrowBlock --blockHeight="18rem" />
-<CarouselView />
 <Spacer --spacer-height="4rem" />
+<AppWindow />
+<ArrowBlock --blockHeight="18rem" />
 <Mission1 />
-<Spacer --spacer-height="4rem" />
-<CarouselBanner />
+<ArrowBlock --blockHeight="18rem" />
