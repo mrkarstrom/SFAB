429af0c584bfaedca95d2aa8a7878cb99ddac00a
Magnus Karström <magnus@yesweb.se>
2022-11-24 20:49:39 +0100

    fix (+page.svelte): Clean up in imports

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <script>
 	import Hero from '$lib/components/hero/Hero.svelte';
 	import DownloadAppPromo1 from '$lib/components/boards/DownloadAppPromo1.svelte';
-	import Spacer from '$lib/components/spacers/Spacer.svelte';
 	import ArrowBlock from '$lib/components/pointers/ArrowBlock.svelte';
 	import AppWindow from '$lib/components/appwindow/AppWindow.svelte';
 	import Mission1 from '$lib/components/mission/Mission1.svelte';
