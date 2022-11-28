1b7989d29e2ef32915f0f52bdc8abc7955596414
Magnus Karström <magnus@yesweb.se>
2022-11-28 16:04:11 +0100

    fix (EasySteps.svelte): Added Three steps container

--- src/lib/components/steps/EasySteps.svelte
+++ src/lib/components/steps/EasySteps.svelte
@@ -1,10 +1,37 @@
-<div class="container h-screen flex flex-col bg-white">
-	<div class="title block text-left w-full">
-		<h2>3 Easy STEPS</h2>
-	</div>
-	<div class="3steps grid grid-cols-3 h-80">
-		<div class="one border-left border-gray" />
-		<div class="two" />
-		<div class="three" />
+<div class="w-full flex flex-row justify-center">
+	<div class="w-full h-auto max-w-[1200px] bg-white flex flex-col">
+		<div class="title-wrap block">
+			<div class="title flex flex-row justify-between">
+				<h2 class="ml-16 py-6 font-medium">3 Easy STEPS</h2>
+			</div>
+		</div>
+		<div class="three-steps-container h-80 w-full flex items-center justify-center">
+			<div class="three-steps grid grid-cols-3 w-[90%] h-full ">
+				<div
+					class="one-box border-slate-500 border-l flex items-center justify-center border-left space-x-7"
+				>
+					<div class="one border rounded-full w-16 h-16 p-0 flex items-center justify-center">
+						<h4>01</h4>
+					</div>
+					<h3 class="whitespace-normal w-48">Open App Store/ Google Play</h3>
+				</div>
+				<div
+					class="two-box border-slate-500 border-x-2 flex items-center justify-center border-left space-x-7"
+				>
+					<div class="two border rounded-full w-16 h-16 p-0 flex items-center justify-center">
+						<h4>02</h4>
+					</div>
+					<h3 class="whitespace-normal w-48">Find SlowFashion App</h3>
+				</div>
+				<div
+					class="three-box border-slate-500 border-r flex items-center justify-center border-left space-x-7"
+				>
+					<div class="three border rounded-full w-16 h-16 p-0 flex items-center justify-center">
+						<h4>03</h4>
+					</div>
+					<h3 class="whitespace-normal w-48">Tap Install</h3>
+				</div>
+			</div>
+		</div>
 	</div>
 </div>
