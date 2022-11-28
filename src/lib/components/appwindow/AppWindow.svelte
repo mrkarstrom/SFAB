808fb824a90cef51adf6cce134055c3ff98173e5
Magnus Karström <magnus@yesweb.se>
2022-11-28 12:24:34 +0100

    feat (AppWindow.svelte): Fixing the left side with Week Fashion

--- src/lib/components/appwindow/AppWindow.svelte
+++ src/lib/components/appwindow/AppWindow.svelte
@@ -1,19 +1,31 @@
-<div class="w-full h-[1500px] min-w-[1024px] bg-secondary flex flex-col">
-	<div class="title-wrap block">
-		<div class="title flex flex-row justify-between">
-			<h2 class="ml-16 py-6">A WINDOW into the App</h2>
-			<h4 class="pt-8 pr-8">View All
-					--->
-			</h4>
+<div class="w-full flex flex-row justify-center">
+	<div class="w-full h-auto max-w-[1200px] bg-secondary flex flex-col">
+		<div class="title-wrap block">
+			<div class="title flex flex-row justify-between">
+				<h2 class="ml-16 py-6 font-medium">A WINDOW into the App</h2>
+				<h5 class="pt-8 pr-8">View All ---></h5>
+			</div>
+		</div>
+		<div class="content-container grid grid-cols-2 h-auto">
+			<div class="left-side flex flex-col items-center w-full">
+				<div class="DG-card h-auto p-5 w-full">
+					<div class="title-text font-medium leading-6 pb-1">
+						<h4 class="w-60 whitespace-normal ">DANCING in Dolce & Gabbana</h4>
+					</div>
+					<img src="https://yesweb.se/sfab/img/mockup/desert-man.png" alt="Desert Man" />
+					<h6 class="pt-1.5">Sweden</h6>
+					<h5>Spring 2023</h5>
+				</div>
+				<div class="WF-card h-auto w-[60%] pt-20 pb-10">
+					<div class="title-text font-medium leading-6 pb-1">
+						<h4 class="">WEEK Fashion</h4>
+					</div>
+					<img src="https://yesweb.se/sfab/img/mockup/chanel-shoe.png" alt="Chanel Shoe" />
+					<h6 class="pt-1.5">Sweden</h6>
+					<h5>Spring 2023</h5>
+				</div>
+			</div>
+			<div class="right-side bg-black h-full w-full">Hello</div>
 		</div>
 	</div>
-	<div class="image-container">
-		<div class="left-side">
-
-		</div>
-		<div class="right-side">
-			
-		</div>
-	</div>
-
 </div>
