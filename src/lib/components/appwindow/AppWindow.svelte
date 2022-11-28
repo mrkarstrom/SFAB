e3d9cfb7b14844da97264427214cedfb7ee12766
Magnus Karström <magnus@yesweb.se>
2022-11-28 13:43:48 +0100

    feat (AppWindow.svelte): Added SF-card to block

--- src/lib/components/appwindow/AppWindow.svelte
+++ src/lib/components/appwindow/AppWindow.svelte
@@ -8,7 +8,7 @@
 		</div>
 		<div class="content-container grid grid-cols-2 h-auto">
 			<div class="left-side flex flex-col items-center w-full">
-				<div class="DG-card h-auto p-5 w-full">
+				<div class="DG-card h-auto p-4 w-full">
 					<div class="title-text font-medium leading-6 pb-1">
 						<h4 class="w-60 whitespace-normal ">DANCING in Dolce & Gabbana</h4>
 					</div>
@@ -16,16 +16,37 @@
 					<h6 class="pt-1.5">Sweden</h6>
 					<h5>Spring 2023</h5>
 				</div>
-				<div class="WF-card h-auto w-[60%] pt-20 pb-10">
+				<div class="WF-card h-auto w-[60%] pt-24 pb-10">
 					<div class="title-text font-medium leading-6 pb-1">
-						<h4 class="">WEEK Fashion</h4>
+						<h4 class="pb-1">WEEK Fashion</h4>
 					</div>
 					<img src="https://yesweb.se/sfab/img/mockup/chanel-shoe.png" alt="Chanel Shoe" />
-					<h6 class="pt-1.5">Sweden</h6>
-					<h5>Spring 2023</h5>
+					<h6 class="pt-1.5">Stockholm</h6>
+					<h5>February 2023</h5>
 				</div>
 			</div>
-			<div class="right-side bg-black h-full w-full">Hello</div>
+			<div class="right-side grid grid-rows-2">
+				<div class="SF-container flex justify-end pt-12 pr-4 w-full ">
+					<div class="SF-card h-auto w-[60%]">
+						<div class="title-text font-medium leading-6 pb-1">
+							<h4 class="pb-1">STYLE & Fashion</h4>
+						</div>
+						<img src="https://yesweb.se/sfab/img/mockup/bike-man.png" alt="Bike Man" />
+						<h6 class="pt-1.5">Sweden</h6>
+						<h5>Winter 2022</h5>
+					</div>
+				</div>
+				<div class="OE-container flex flex-col items-center justify-start border-red-500 border">
+					<div class="OE-card h-auto w-[60%] pt-24 pb-10">
+						<div class="title-text font-medium leading-6 pb-1">
+							<h4 class="pb-1">WEEK Fashion</h4>
+						</div>
+						<img src='https://yesweb.se/sfab/img/mockup/woman-gray-coat.png' alt="Chanel Shoe" />
+						<h6 class="pt-1.5">Stockholm</h6>
+						<h5>February 2023</h5>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
