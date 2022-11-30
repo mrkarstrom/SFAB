05db8f0da2147ecdaab17724239c27362a6b195f
Magnus Karström <magnus@yesweb.se>
2022-11-30 12:42:29 +0100

    fix (BeInspired.svelte): Fixed the text-boxes in gridboxes

--- src/lib/components/boards/BeInspired.svelte
+++ src/lib/components/boards/BeInspired.svelte
@@ -1,5 +1,5 @@
 <div class="be-inspired-container w-full h-auto flex justify-center">
-	<div class="be-inspired-box w-10/12">
+	<div class="be-inspired-box px-20 w-full">
 		<div class="be-inspired-title block">
 			<h2>Be INSPIRED</h2>
 			<p class="w-[600px] whitespace-normal">
@@ -7,19 +7,40 @@
 				temporibus dolorum!
 			</p>
 		</div>
-		<div class="image-container grid grid-cols-3 gap-4 h-80 pt-8">
+		<div class="image-container grid grid-cols-3 gap-4 h-96 pt-8">
 			<div
 				class="first bg-cover bg-center"
 				style="background-image: url('https://yesweb.se/sfab/img/mockup/man-watch.png')"
-			/>
+			>
+				<div
+					class="textbox flex flex-col items-start align-center w-9/10 h-auto bg-accent m-3 py-1 px-2 rounded"
+				>
+					<h5 class="font-bold">Prada linien drawstring bag</h5>
+					<h6>Made by friendlyseamstress</h6>
+				</div>
+			</div>
 			<div
 				class="first bg-cover bg-center"
 				style="background-image: url('https://yesweb.se/sfab/img/mockup/woman-coat.png')"
-			/>
+			>
+				<div
+					class="textbox flex flex-col items-start align-center  w-9/10 h-auto bg-accent m-3 py-1 px-2 rounded"
+				>
+					<h5 class="font-bold">Prada linien drawstring bag</h5>
+					<h6>Made by friendlyseamstress</h6>
+				</div>
+			</div>
 			<div
 				class="first bg-cover bg-center"
 				style="background-image: url('https://yesweb.se/sfab/img/mockup/coat-belt.png')"
-			/>
+			>
+				<div
+					class="textbox flex flex-col items-start align-center w-9/10 h-auto bg-accent m-3 py-1 px-2 rounded"
+				>
+					<h5 class="font-bold">Prada linien drawstring bag</h5>
+					<h6>Made by friendlyseamstress</h6>
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
