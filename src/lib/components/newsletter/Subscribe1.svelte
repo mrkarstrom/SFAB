b8b906c77fdee2de5086319ba3856e96ad51c3f0
Magnus Karström <magnus@yesweb.se>
2022-11-30 13:07:07 +0100

    fix (BeInspired.svelte): Fixed the text-boxes in gridboxes

--- src/lib/components/newsletter/Subscribe1.svelte
+++ src/lib/components/newsletter/Subscribe1.svelte
@@ -1,9 +1,8 @@
-<div class="newsletter-container w-full h-auto">
-	<section class="flex flex-col align-start pl-8">
+<div class="newsletter-container w-full h-auto border-t-2 border-gray-400 ">
+	<section class="flex flex-col align-start pl-20">
 		<div class="newsletter-signup">
 			<h2>NEWSLETTER</h2>
-			<input type="text" name="inputName" id="inputName"/>
-
+			<input type="text" name="inputName" id="inputName" />
 		</div>
 	</section>
 </div>
