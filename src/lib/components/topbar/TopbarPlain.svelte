82b1294c0cc5b1fcdfd0be81cb5ed6c42b06f60d
Magnus Karström <magnus@yesweb.se>
2022-12-02 12:04:55 +0100

    fix (body, html): fixing some layout properties

--- src/lib/components/topbar/TopbarPlain.svelte
+++ src/lib/components/topbar/TopbarPlain.svelte
@@ -2,9 +2,9 @@
 	export let currentLang = 'EN';
 </script>
 
-<div class="flex flex-col w-screen h-[40px] bg-secondary  items-center justify-center">
+<div class="relative flex w-full h-[40px] bg-secondary items-center justify-center">
 	<p class="hidden md:flex font-normal text-sm">
-		We Are The Best Marketplace for Pre-Owned High Quality Fashion
+		We Are The Marketplace for Pre-Owned High Quality Fashion
 	</p>
 	<button class="absolute right-8">{currentLang}</button>
 </div>
