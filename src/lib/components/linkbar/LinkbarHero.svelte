c94a404b6c2a5aab6bf01520caf51368d0aa8ad0
Magnus Karström <magnus@yesweb.se>
2022-12-07 14:08:41 +0100

    Merging

--- src/lib/components/linkbar/LinkbarHero.svelte
+++ src/lib/components/linkbar/LinkbarHero.svelte
@@ -1,7 +1,5 @@
 <div class="absolute left-8">
-	<div
-		class="wrapper hidden md:flex flex-row items-center justify-center mt-16 space-y-12 h-64 w-10"
-	>
+	<div class="wrapper flex flex-row items-center justify-center mt-16 space-y-12 h-64 w-10">
 		<ul class="space-y-5">
 			<li class="flex items-center justify-center w-10 h-10 border border-white rounded-full">
 				<a href="https://linkedin.com">
