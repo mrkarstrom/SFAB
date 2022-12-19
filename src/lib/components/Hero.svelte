591e9702fc56d9d204b0c004c6a1679623d35c88
Magnus Karström <magnus@yesweb.se>
2022-12-19 16:21:19 +0100

    fix (TopbarPlain.svelte): Making the hero image responsive

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -4,11 +4,11 @@
 </script>
 
 <div
-	class="backdrop bg-no-repeat bg-center m-0 p-0 h-[800px] w-full overflow-x-hidden"
+	class="backdrop bg-no-repeat bg-cover bg-center m-0 p-0 h-[800px] overflow-x-hidden"
 	style="background-image: url('https://yesweb.se/sfab/img/mockup/hero-couple.png') "
 >
 	<LinkbarHero />
-	<div class="box flex flex-col mt-48 items-center justify-center">
+	<div class=" box flex flex-col mt-48 items-center justify-center">
 		<h4 class="mb-1 text-white uppercase">Be the change</h4>
 		<h1 class="mb-10 text-white font-['Steelfish'] uppercase ">Slowfashion</h1>
 		<ArrowDown --arrowColor="white" --circleColor="white" />
--- src/lib/components/TopbarPlain.svelte
+++ src/lib/components/TopbarPlain.svelte
@@ -2,9 +2,22 @@
 	export let currentLang = 'EN';
 </script>
 
-<div class="relative flex w-full h-[40px] bg-secondary items-center justify-center">
+<div
+	class="grid w-full h-[40px] bg-secondary grid-cols-[50px_auto_50px] items-center justify-between"
+>
+	<div class="" />
+	<div class="">
+		<p class="flex font-normal text-sm">
+			We Are The Marketplace for Pre-Owned High Quality Fashion
+		</p>
+	</div>
+	<div class=""><button class="">{currentLang}</button></div>
+</div>
+<!-- 
+
+<div class="flex w-auto h-[40px] bg-secondary items-center justify-center">
 	<p class="hidden md:flex font-normal text-sm">
 		We Are The Marketplace for Pre-Owned High Quality Fashion
 	</p>
-	<button class="absolute right-8">{currentLang}</button>
-</div>
+	<button class="right-[10px]">{currentLang}</button>
+</div> -->
