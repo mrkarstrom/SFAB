bb9db081346f030b361dfa3d0cfaccfe2bdf01cb
Magnus Karström <magnus@yesweb.se>
2023-01-21 17:33:45 +0100

    feat (DownloadApp.svelte): We have a working carousel!

--- src/lib/components/DownloadApp.svelte
+++ src/lib/components/DownloadApp.svelte
@@ -1,10 +1,44 @@
+<script>
+	import { flip } from 'svelte/animate';
+	export let imageWidth = 530;
+	export let imageSpacing = '0px';
+	export let transitionSpeed = 500;
+
+	let images = [
+		{ path: 'https://yesweb.se/sfab/img/mockup/aesthetic-phone2.jpeg', id: 'image1' },
+		{ path: 'https://yesweb.se/sfab/img/mockup/aesthetic-phone2.jpeg', id: 'image2' },
+		{ path: 'https://yesweb.se/sfab/img/mockup/aesthetic-phone2.jpeg', id: 'image3' },
+		{ path: 'https://yesweb.se/sfab/img/mockup/aesthetic-phone2.jpeg', id: 'image4' },
+		{ path: 'https://yesweb.se/sfab/img/mockup/aesthetic-phone2.jpeg', id: 'image5' }
+	];
+
+	const rotateLeft = (e) => {
+		const transitioningImage = images[images.length - 1];
+		document.getElementById(transitioningImage.id).style.opacity = 0;
+		images = [images[images.length - 1], ...images.slice(0, images.length - 1)];
+		setTimeout(() => {
+			document.getElementById(transitioningImage.id).style.opacity = 1;
+		}, transitionSpeed);
+	};
+
+	const rotateRight = (e) => {
+		const transitioningImage = images[0];
+		document.getElementById(transitioningImage.id).style.opacity = 0;
+		images = [...images.slice(1, images.length), images[0]];
+		setTimeout(() => {
+			document.getElementById(transitioningImage.id).style.opacity = 1;
+		}, transitionSpeed);
+	};
+</script>
+
 <div id="spacer" class="w-full h-[400px] flex justify-center items-end lg:flex lg:h-[200px] ">
 	<div
 		id="wrapper"
-		class="relative bottom-[10px] w-[92%] h-[400px] drop-shadow-xl lg:bottom-[50px] lg:flex"
+		class="relative bottom-[10px] w-[930px] h-[395px] drop-shadow-xl lg:bottom-[50px] lg:flex"
 	>
 		<div id="box-wrap" class="flex items-center justify-center w-full h-full">
 			<div
+				on:click={rotateLeft}
 				id="left-box-arrow"
 				class="hidden lg:flex items-center justify-center h-[90%] w-[5rem] bg-secondary rounded-lg"
 			>
@@ -44,15 +78,23 @@
 					</button>
 				</div>
 				<div class="lg:col-span-2 lg:col-start-2">
-					<img
-						id="aestetic-phone"
-						class="object-fill"
-						src="https://yesweb.se/sfab/img/mockup/aesthetic-phone2.jpeg"
-						alt=""
-					/>
+					<div id="carousel-container">
+						<div id="carousel-images">
+							{#each images as image (image.id)}
+								<img
+									src={image.path}
+									alt={image.id}
+									id={image.id}
+									style={`width: ${imageWidth}px; margin: 0 ${imageSpacing};`}
+									animate:flip={{ duration: transitionSpeed }}
+								/>
+							{/each}
+						</div>
+					</div>
 				</div>
 			</div>
 			<div
+				on:click={rotateRight}
 				class="right-box-arrow  hidden lg:flex items-center justify-center h-[90%] w-[5rem] bg-secondary rounded-lg"
 			>
 				<svg xmlns="http://www.w3.org/2000/svg" height="1rem" fill="black" viewBox="0 0 512 512"
@@ -65,3 +107,19 @@
 		</div>
 	</div>
 </div>
+
+<style>
+	#carousel-container {
+		width: 100%;
+		position: relative;
+		display: flex;
+		flex-direction: column;
+		overflow-x: hidden;
+	}
+
+	#carousel-images {
+		display: flex;
+		justify-content: center;
+		flex-wrap: nowrap;
+	}
+</style>
