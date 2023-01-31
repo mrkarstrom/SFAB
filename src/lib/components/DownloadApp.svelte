be0ec327f01fe32148118dc017e68fbd7fccb46b
Magnus Karström <magnus@yesweb.se>
2023-01-31 17:54:52 +0100

    fix (DownloadApp.svelte): Got the Carousel working properly!

--- src/lib/components/DownloadApp.svelte
+++ src/lib/components/DownloadApp.svelte
@@ -1,8 +1,4 @@
 <script>
-	import Card1 from './Card1.svelte';
-	import Card2 from './Card2.svelte';
-	import Card3 from './Card3.svelte';
-	import Card4 from './Card4.svelte';
 	import * as animateScroll from 'svelte-scrollto';
 </script>
 
@@ -24,6 +20,7 @@
 
 		<!-- Carousel start -->
 		<div class="carousel w-full h-full z-10">
+			<!-- Slide 1 Start -->
 			<div id="slide1" class="carousel-item items-center">
 				<div class="flex items-center justify-center w-full h-full">
 					<a
@@ -71,6 +68,157 @@
 					/>
 				</div>
 			</div>
+			<!-- Slide1 End -->
+
+			<!-- Slide 2 Start -->
+			<div id="slide2" class="carousel-item items-center">
+				<div class="flex items-center justify-center w-full h-full">
+					<a
+						class="flex items-center justify-center h-[90%] w-[70px] bg-inherit rounded-lg z-20"
+						href="#slide1"
+						on:click={() => animateScroll.scrollTo({ element: '#slide1', offset: -300 })}
+					/>
+
+					<div
+						id="center-box"
+						class="h-full w-[810px] gap-0 grid grid-rows-1 grid-cols-3 content-center "
+					>
+						<div
+							id="left-box-part"
+							class="bg-white flex flex-col items-center justify-between w-full h-full rounded-lg lg:pt-24 lg:pb-6 lg:rounded-lg"
+						>
+							<div
+								id=" download-icon"
+								class="w-full h-full flex flex-col items-center justify-center pt-12 lg:w-[90px] lg:h-[90px]"
+							>
+								<img
+									src="https://yesweb.se/sfab/img/mockup/download-app-icon.svg"
+									width="120px"
+									alt=""
+								/>
+								<h6 class="hidden text-center whitespace-nowrap pt-14 lg:flex">
+									Honest | Smooth | Desirable
+								</h6>
+							</div>
+							<button
+								id="join-button"
+								class="bg-accent flex items-center justify-center h-10 w-60 rounded-md uppercase text-primary hover:bg-accent"
+							>
+								<h5>Join Slowfashion</h5>
+							</button>
+						</div>
+						<div class="h-full w-full col-start-2 col-span-2 items-center overflow-y-hidden">
+							<img src="src\lib\images\aesthetic-phone.png" alt="" />
+						</div>
+					</div>
+					<a
+						class="flex items-center justify-center h-[90%] w-[70px] bg-inherit rounded-lg z-20"
+						href="#slide3"
+						on:click={() => animateScroll.scrollTo({ element: '#slide3', offset: -300 })}
+					/>
+				</div>
+			</div>
+			<!-- Slide2 End -->
+
+			<!-- Slide 3 Start -->
+			<div id="slide3" class="carousel-item items-center">
+				<div class="flex items-center justify-center w-full h-full">
+					<a
+						class="flex items-center justify-center h-[90%] w-[70px] bg-inherit rounded-lg z-20"
+						href="#slide2"
+						on:click={() => animateScroll.scrollTo({ element: '#slide2', offset: -300 })}
+					/>
+
+					<div
+						id="center-box"
+						class="h-full w-[810px] gap-0 grid grid-rows-1 grid-cols-3 content-center "
+					>
+						<div
+							id="left-box-part"
+							class="bg-white flex flex-col items-center justify-between w-full h-full rounded-lg lg:pt-24 lg:pb-6 lg:rounded-lg"
+						>
+							<div
+								id=" download-icon"
+								class="w-full h-full flex flex-col items-center justify-center pt-12 lg:w-[90px] lg:h-[90px]"
+							>
+								<img
+									src="https://yesweb.se/sfab/img/mockup/download-app-icon.svg"
+									width="120px"
+									alt=""
+								/>
+								<h6 class="hidden text-center whitespace-nowrap pt-14 lg:flex">
+									Honest | Smooth | Desirable
+								</h6>
+							</div>
+							<button
+								id="join-button"
+								class="bg-accent flex items-center justify-center h-10 w-60 rounded-md uppercase text-primary hover:bg-accent"
+							>
+								<h5>Join Slowfashion</h5>
+							</button>
+						</div>
+						<div class="h-full w-full col-start-2 col-span-2 items-center overflow-y-hidden">
+							<img src="src\lib\images\aesthetic-phone.png" alt="" />
+						</div>
+					</div>
+					<a
+						class="flex items-center justify-center h-[90%] w-[70px] bg-inherit rounded-lg z-20"
+						href="#slide4"
+						on:click={() => animateScroll.scrollTo({ element: '#slide4', offset: -300 })}
+					/>
+				</div>
+			</div>
+			<!-- Slide3 End -->
+
+			<!-- Slide 4 Start -->
+			<div id="slide4" class="carousel-item items-center">
+				<div class="flex items-center justify-center w-full h-full">
+					<a
+						class="flex items-center justify-center h-[90%] w-[70px] bg-inherit rounded-lg z-20"
+						href="#slide3"
+						on:click={() => animateScroll.scrollTo({ element: '#slide3', offset: -300 })}
+					/>
+
+					<div
+						id="center-box"
+						class="h-full w-[810px] gap-0 grid grid-rows-1 grid-cols-3 content-center "
+					>
+						<div
+							id="left-box-part"
+							class="bg-white flex flex-col items-center justify-between w-full h-full rounded-lg lg:pt-24 lg:pb-6 lg:rounded-lg"
+						>
+							<div
+								id=" download-icon"
+								class="w-full h-full flex flex-col items-center justify-center pt-12 lg:w-[90px] lg:h-[90px]"
+							>
+								<img
+									src="https://yesweb.se/sfab/img/mockup/download-app-icon.svg"
+									width="120px"
+									alt=""
+								/>
+								<h6 class="hidden text-center whitespace-nowrap pt-14 lg:flex">
+									Honest | Smooth | Desirable
+								</h6>
+							</div>
+							<button
+								id="join-button"
+								class="bg-accent flex items-center justify-center h-10 w-60 rounded-md uppercase text-primary hover:bg-accent"
+							>
+								<h5>Join Slowfashion</h5>
+							</button>
+						</div>
+						<div class="h-full w-full col-start-2 col-span-2 items-center overflow-y-hidden">
+							<img src="src\lib\images\aesthetic-phone.png" alt="" />
+						</div>
+					</div>
+					<a
+						class="flex items-center justify-center h-[90%] w-[70px] bg-inherit rounded-lg z-20"
+						href="#slide1"
+						on:click={() => animateScroll.scrollTo({ element: '#slide1', offset: -300 })}
+					/>
+				</div>
+			</div>
+			<!-- Slide4 End -->
 		</div>
 		<!-- Carousel end -->
 
