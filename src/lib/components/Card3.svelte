20184297d1d6e64e397432e4fa639f2e94fc4734
Magnus Karström <magnus@yesweb.se>
2023-02-02 17:15:15 +0100

    feat (Card3.svelte): Adding spinner to card3

--- src/lib/components/Card3.svelte
+++ src/lib/components/Card3.svelte
@@ -12,28 +12,28 @@
 	<div
 		id="left-box-part"
 		class="bg-white flex flex-col items-center justify-between w-full h-full rounded-lg lg:pt-24 lg:pb-6 lg:rounded-lg"
-	>
-		<div
-			id=" download-icon"
-			class="w-full h-full flex flex-col items-center justify-center pt-12 lg:w-[90px] lg:h-[90px]"
-		>
-			<img src="https://yesweb.se/sfab/img/mockup/download-app-icon.svg" width="120px" alt="" />
-			<h6 class="hidden text-center whitespace-nowrap pt-14 lg:flex">
-				Honest | Smooth | Desirable
-			</h6>
-		</div>
-		<button
-			id="join-button"
-			class="bg-accent flex items-center justify-center h-10 w-60 rounded-md uppercase text-primary hover:bg-accent"
-		>
-			<h5>Join Slowfashion</h5>
-		</button>
-	</div>
+	/>
 	<div class="h-full w-full col-start-2 col-span-2 items-center overflow-y-hidden">
-		<img
-			src="https://github.com/mrkarstrom/sfab/blob/main/src/lib/images/Card3-image.jpeg?raw=true"
-			alt=""
-		/>
+		<div class="relative h-full w-full col-start-2 col-span-2 items-center overflow-y-hidden">
+			<div class="absolute left-8 bottom-36">
+				<img
+					id="spinning-circle"
+					src="https://github.com/mrkarstrom/sfab/blob/main/src/lib/images/Card3-spinner.png?raw=true"
+					width="110px"
+					alt=""
+				/>
+				<img
+					class="z-10 absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2"
+					src="https://github.com/mrkarstrom/sfab/blob/main/src/lib/images/download-box-icon-card3.png?raw=true"
+					width="40px"
+					alt=""
+				/>
+			</div>
+			<img
+				src="https://github.com/mrkarstrom/sfab/blob/main/src/lib/images/Card3-image.jpeg?raw=true"
+				alt=""
+			/>
+		</div>
 	</div>
 </div>
 <a
@@ -41,3 +41,23 @@
 	href="#slide4"
 	on:click={() => animateScroll.scrollTo({ element: '#slide4', offset: -300 })}
 />
+
+<style>
+	#spinning-circle {
+		animation-name: spinning-circle;
+		animation-duration: 8s;
+		animation-iteration-count: infinite;
+		animation-timing-function: linear;
+	}
+
+	@keyframes spinning-circle {
+		0% {
+			-webkit-transform: rotate(0deg);
+			transform: rotate(0deg);
+		}
+		100% {
+			-webkit-transform: rotate(360deg);
+			transform: rotate(360deg);
+		}
+	}
+</style>
--- src/lib/components/Card4.svelte
+++ src/lib/components/Card4.svelte
@@ -13,29 +13,15 @@
 		id="left-box-part"
 		class="bg-white flex flex-col items-center justify-between w-full h-full rounded-lg lg:pt-24 lg:pb-6 lg:rounded-lg"
 	>
-		
+		<h6 class="hidden text-center w-32 whitespace-wrap pt-14 lg:flex">All within the same app.</h6>
 		<button
 			id="join-button"
-			class="bg-accent flex items-center justify-center h-10 w-60 rounded-md uppercase text-primary hover:bg-accent"
+			class="bg-accent flex items-center justify-center h-10 w-60 rounded-md uppercase text-primary hover:bg-black hover:text-white transition duration-300 hover:ease-in-out"
 		>
 			<h5>Join Slowfashion</h5>
 		</button>
 	</div>
 	<div class="h-full w-full col-start-2 col-span-2 items-center overflow-y-hidden">
-		<div class="relative flex items-center justify-center">
-				<img
-					id="spinning-circle"
-					src="https://github.com/mrkarstrom/sfab/blob/main/src/lib/images/Card3-spinner.png?raw=true"
-					width="120px"
-					alt=""
-				/>
-				<img
-					class="z-10 absolute"
-					src="https://github.com/mrkarstrom/sfab/blob/main/src/lib/images/download-box-icon-card3.png?raw=true"
-					width="40px"
-					alt=""
-				/>
-			</div>
 		<img
 			src="https://github.com/mrkarstrom/sfab/blob/main/src/lib/images/Card4-image.jpeg?raw=true"
 			alt=""
