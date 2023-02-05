a8c3c74f95d23136db5be71c3394b0927bf9e807
Magnus Karström <magnus@yesweb.se>
2023-02-05 20:12:50 +0100

    Saving this to try another way

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,10 +9,12 @@
 	import AppWindow from '$lib/components/AppWindow.svelte';
 	import Discover from '$lib/components/Discover.svelte';
 	import Mission1 from '$lib/components/Mission1.svelte';
-	import Discover from '$lib/components/Discover.svelte';
 	import Subscribe1 from '$lib/components/Subscribe1.svelte';
 	import Footer from '$lib/components/Footer.svelte';
-
+	import { blockA } from '../stores/blocks';
+	import { blockB } from '../stores/blocks';
+	import { blockC } from '../stores/blocks';
+	import { blockD } from '../stores/blocks';
 	let scroll;
 </script>
 
@@ -24,26 +26,49 @@
 <DownloadApp />
 
 <ArrowBlock --blockHeight="14rem" --circleColor="black" --arrowColor="black" />
-{#if scroll >= 590}
-	<div class="block" transition:fly={{ y: 200, duration: 1500 }}>
+{#if scroll >= 600}
+	{#if $blockA === true}
+		<div class="block" transition:fly={{ y: 800, duration: 1500 }}>
+			<AppWindow />
+		</div>
+	{:else}
 		<AppWindow />
-	</div>
+	{/if}
 {/if}
 <ArrowBlock --blockHeight="20rem" --circleColor="black" --arrowColor="black" />
-{#if scroll >= 2700}
-	<div class="block" transition:fly={{ y: 200, duration: 1500 }}>
+{#if scroll >= 2500}
+	{#if $blockB === true}
+		<div class="block" transition:fly={{ y: 600, duration: 1500 }}>
+			<EasySteps />
+		</div>
+	{:else}
 		<EasySteps />
-	</div>
+	{/if}
 {/if}
 <ArrowBlock --blockHeight="20rem" --circleColor="black" --arrowColor="black" />
 
 {#if scroll >= 3900}
-	<div class="block" transition:fly={{ y: 200, duration: 1500 }}>
+	{#if $blockC === true}
+		<div class="block" transition:fly|once={{ y: 600, duration: 1500 }}>
+			<Discover />
+		</div>
+		{($blockB = false)}
+	{:else}
 		<Discover />
-	</div>
+	{/if}
 {/if}
 <ArrowBlock --blockHeight="20rem" --circleColor="black" --arrowColor="black" />
-<Mission1 />
+{#if scroll >= 4200}
+	{#if $blockD === true}
+		<div class="block" transition:fly={{ y: 600, duration: 1500 }}>
+			<Mission1 />
+			{($blockC = false)}
+		</div>
+	{:else}
+		<Mission1 />
+	{/if}
+{/if}
 <ArrowBlock --blockHeight="20rem" --circleColor="black" --arrowColor="black" />
+{($blockD = false)}
 <Subscribe1 />
 <Footer />
---
+++ src/stores/blocks.js
@@ -0,0 +1,6 @@
+import { writable } from 'svelte/store';
+
+export const blockA = writable(true);
+export const blockB = writable(true);
+export const blockC = writable(true);
+export const blockD = writable(true);
