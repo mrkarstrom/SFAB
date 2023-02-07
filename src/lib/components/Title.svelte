82315ac8094991c1f26d6d1f6db8cbf3200731b4
Magnus Karström <magnus@yesweb.se>
2023-02-07 14:28:45 +0100

    fix (AppWindow): Bouncing right View all, Title component

--- src/lib/components/Title.svelte
+++ src/lib/components/Title.svelte
@@ -12,7 +12,7 @@
 >
 	{#if isInView}
 		<div in:fly={{ x: 50, duration: 1500, delay: 1000 }}>
-			<p class="text-4xl ml-16 py-6 font-normal">A WINDOW into the App</p>
+			<slot/>
 		</div>
 	{/if}
 </div>
