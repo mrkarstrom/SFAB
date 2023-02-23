06bdf029effc8646c40bf1faf9adb748e12a3226
Magnus Karström <magnus@yesweb.se>
2023-02-23 10:55:20 +0100

    fix (Modal.svelte): Not really fixed but a scroll to bottom

--- src/lib/components/Modal.svelte
+++ src/lib/components/Modal.svelte
@@ -1,20 +1,20 @@
 <script>
 	import { showModal } from '../../stores/store';
+	const handleClick = () => {
+		window.scrollTo(0, document.body.scrollHeight);
+		showModal.set(false);
+	};
 </script>
 
 <div
 	id="backdrop"
-	on:click={() => {
-		showModal.set(false);
-	}}
-	class="w-screen h-screen fixed opacity-[0.9] bg-black z-50"
+	on:click={handleClick}
+	class="w-screen h-full absolute bottom-0 opacity-[0.9] bg-black z-50"
 >
 	<div class="w-full h-full flex items-center justify-center">
 		<div
 			id="popup-modal"
-			on:click={() => {
-				showModal.set(false);
-			}}
+			on:click={handleClick}
 			class="w-1/2 h-80 bg-secondary flex flex-col items-center justify-center rounded p-6"
 		>
 			<h2 class="p-4">About Us</h2>
