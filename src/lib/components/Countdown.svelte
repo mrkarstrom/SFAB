cee470b69274f8c40b8f90389f1d00a444da056d
Magnus Karström <magnus@yesweb.se>
2023-03-03 22:55:58 +0100

    fix (Countdown.svelte): Making the number-boxes 80px

--- src/lib/components/Countdown.svelte
+++ src/lib/components/Countdown.svelte
@@ -1,7 +1,7 @@
 <script>
 	import { onMount } from 'svelte';
 	let time = new Date();
-	const goalDate = new Date('May 07 2023 00:00:00');
+	const goalDate = new Date('May 07 2023 01:12:00');
 
 	$: diff = goalDate - time;
 	$: d = Math.floor(diff / 1000 / 60 / 60 / 24);
@@ -25,19 +25,31 @@
 </script>
 
 <div class="w-full h-52 flex flex-col items-center justify-center">
-	<p class="text-[2.5rem] whitespace-normal text-center w-60 leading-none ">Countdown to launch</p>
-	<div class="grid grid-cols-3 grid-rows-1 gap-0 p-0 m-0 w-auto h-full">
+	<p class="text-[2.5rem] whitespace-normal text-center leading-none ">Countdown to launch</p>
+	<div class="grid grid-cols-3 grid-rows-1 gap-x-10 pb-10 m-0 w-[80%] h-full">
 		<div class="flex flex-col items-center">
-			<h2 class="font-['Digit']">{days}</h2>
-			<small>days</small>
+			<p
+				class="font-['Digit'] min-w-[80px] text-center text-white text-[4rem] bg-secondary my-4 rounded-md p-2 leading-none"
+			>
+				{days}
+			</p>
+			<small>DAYS</small>
 		</div>
 		<div class="flex flex-col items-center">
-			<h2 class="font-['Digit']">{hours}</h2>
-			<small>hours</small>
+			<p
+				class="font-['Digit'] min-w-[80px] text-center text-white text-[4rem] bg-secondary my-4 rounded-md p-2 leading-none"
+			>
+				{hours}
+			</p>
+			<small>HOURS</small>
 		</div>
 		<div class="flex flex-col items-center">
-			<h2 class="font-['Digit']">{minutes}</h2>
-			<small>minutes</small>
+			<p
+				class="font-['Digit'] min-w-[80px] text-center text-white text-[4rem] bg-secondary my-4 rounded-md p-2 leading-none"
+			>
+				{minutes}
+			</p>
+			<small>MINUTES</small>
 		</div>
 	</div>
 </div>
