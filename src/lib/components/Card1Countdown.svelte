ec7222a234abe9378a29bf0f74f3d4ae0c63bf0e
Magnus Karström <magnus@yesweb.se>
2023-03-03 21:18:54 +0100

    fix (Card): Making the Countdown component

--- src/lib/components/Card1Countdown.svelte
+++ src/lib/components/Card1Countdown.svelte
@@ -1,3 +1,7 @@
+<script>
+	import Countdown from './Countdown.svelte';
+</script>
+
 <div
 	id="container-box"
 	class="h-full w-auto lg:w-[810px] gap-0 grid grid-rows-1 grid-cols-3 content-center z-30"
@@ -24,11 +28,10 @@
 			</div>
 			<h6 class="text-center whitespace-nowrap pt-14 lg:flex">Honest | Smooth | Desirable</h6>
 		</div>
-		<button
-			class="has-pointer-events bg-accent flex items-center justify-center h-10 w-60 rounded-md uppercase text-primary hover:bg-black hover:text-white transition duration-300 hover:ease-in-out"
-		>
-			<h5>Join Slowfashion</h5>
-		</button>
+		<div>
+			<h3 >Countdown to Launch</h3>
+			<Countdown />
+		</div>
 	</div>
 	<div class="h-full w-full col-start-2 col-span-2 items-center overflow-y-hidden">
 		<img
