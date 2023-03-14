d3078926629ed477a0c6fa88b30a80a05a70feb2
Magnus Karström <magnus@yesweb.se>
2023-03-14 17:28:03 +0100

    feat (Discover.svelte): Trying the card-component from daisyui

--- src/lib/components/Discover.svelte
+++ src/lib/components/Discover.svelte
@@ -13,13 +13,19 @@
 	}}
 >
 	{#if isInView}
-		<div in:fly={{ y: 500, duration: 1500 }} class="flex flex-col items-center justify-center w-full h-full">
+		<div
+			in:fly={{ y: 500, duration: 1500 }}
+			class="flex flex-col items-center justify-center w-full h-full"
+		>
 			<div
 				id="discover-container"
 				class=" flex items-center justify-center w-full h-full
 												"
 			>
-				<div id="discover-box" class="flex flex-col items-center justify-center w-full h-full px-20">
+				<div
+					id="discover-box"
+					class="flex flex-col items-center justify-center w-full h-full px-20"
+				>
 					<div id="discover-title" class="w-full">
 						<Title>
 							<p class="text-4xl font-normal text-black">DISCOVER in the app</p>
@@ -39,7 +45,70 @@
 													md:h-96
 													"
 					>
-						<div
+						<div class="card w-92 rounded-sm items-center">
+							<div
+								id="textbox1"
+								class="hidden absolute left-2 right-2 flex-col flex-start items-center justify-center h-auto bg-accent mx-3 mt-2 py-2 px-4 rounded 
+								md:flex
+								"
+							>
+								<p class="w-full font-semibold text-xs text-left">
+									Michael Kors watches in beautiful gold
+								</p>
+								<p class="hidden w-full font-thin text-xs text-left lg:flex">
+									It's easy to elevate any outfit and add a bit of flair
+								</p>
+							</div>
+							<figure>
+								<img
+									src="https://github.com/mrkarstrom/sfab/blob/main/src/lib/images/perfume.svg?raw=true"
+									alt="Perfume"
+								/>
+							</figure>
+						</div>
+						<div class="card w-92 rounded-sm items-center">
+							<div
+								id="textbox2"
+								class="hidden absolute left-2 right-2 flex-col flex-start items-center justify-center h-auto bg-accent mx-3 mt-2 py-2 px-4 rounded 
+								md:flex
+								"
+							>
+								<p class="w-full font-semibold text-xs text-left">
+									Michael Kors watches in beautiful gold
+								</p>
+								<p class="hidden w-full font-thin text-xs text-left lg:flex">
+									It's easy to elevate any outfit and add a bit of flair
+								</p>
+							</div>
+							<figure>
+								<img
+									src="https://github.com/mrkarstrom/sfab/blob/main/src/lib/images/perfume.svg?raw=true"
+									alt="Perfume"
+								/>
+							</figure>
+						</div>
+						<div class="card w-92 rounded-sm items-center">
+							<div
+								id="textbox3"
+								class="hidden absolute left-2 right-2 flex-col flex-start items-center justify-center h-auto bg-accent mx-3 mt-2 py-2 px-4 rounded 
+								md:flex
+								"
+							>
+								<p class="w-full font-semibold text-xs text-left">
+									Michael Kors watches in beautiful gold
+								</p>
+								<p class="hidden w-full font-thin text-xs text-left lg:flex">
+									It's easy to elevate any outfit and add a bit of flair
+								</p>
+							</div>
+							<figure>
+								<img
+									src="https://github.com/mrkarstrom/sfab/blob/main/src/lib/images/perfume.svg?raw=true"
+									alt="Perfume"
+								/>
+							</figure>
+						</div>
+						<!-- <div
 							id="image-box1"
 							class="relative aspect-square m-5 bg-[url('https://github.com/mrkarstrom/sfab/blob/main/src/lib/images/perfume.svg?raw=true')] bg-cover 
 							md:w-6xl
@@ -61,8 +130,8 @@
 									It's easy to elevate any outfit and add a bit of flair
 								</p>
 							</div>
-						</div>
-						<div
+						</div> -->
+						<!-- <div
 							id="image-box2"
 							class="relative w-2/3 m-5 aspect-square bg-[url('https://github.com/mrkarstrom/sfab/blob/main/src/lib/images/bag-watch-coat.svg?raw=true')] bg-cover  
 							md:w-72
@@ -102,7 +171,7 @@
 								alt="Gucci Bag"
 								class="w-full"
 							/> -->
-						</div>
+						<!-- </div>  -->
 					</div>
 				</div>
 			</div>
