ad157cf1ace80ebb132462c40cd3a0cfbc9bf455
Magnus Karström <magnus@yesweb.se>
2023-03-23 13:14:37 +0100

    fix (EasySteps.svelte): Tuning som of the layouts

--- src/lib/components/EasySteps.svelte
+++ src/lib/components/EasySteps.svelte
@@ -26,14 +26,14 @@
 
 					<div
 						id="two-steps"
-						class="grid grid-rows-2 w-full grid-cols-1 h-full justify-items-center content-center
+						class="grid grid-rows-2 w-full grid-cols-1 divide-y-[50%] h-full justify-items-center content-center
 													lg:grid-cols-2
 													lg:grid-rows-1  
 													lg:max-w-[600px]"
 					>
 						<div
 							id="box-one"
-							class="border-black border-b flex gap-8 items-center justify-center h-full w-96 max-w-96 
+							class=" flex gap-8 items-center justify-between h-full w-[300px] m-8
 										lg:items-end
 										lg:border-x
 										lg:space-x-6
@@ -46,26 +46,26 @@
 							>
 								<h4>1</h4>
 							</div>
-							<p class="text-lg whitespace-normal w-[180px] hover:font-medium lg:w-40">
+							<p class="text-lg whitespace-normal  hover:font-medium lg:w-40">
 								Search for<br /> SLOWFASHION in App Store or Google Play.
 							</p>
 						</div>
 						<div
 							id="box-two"
-							class="border-black border-b flex items-end justify-center min-h-[130px] pb-8 space-x-6 w-full
-										md:border-r
-										md:border-y-0
+							class="flex gap-8 items-center justify-between h-full w-[300px] m-8 
+										lg:items-end
+										lg:border-x
+										lg:space-x-6
+										lg:border-y-0
 										"
 						>
 							<div
 								id="ring-two"
-								class="border-black border mb-4 w-16 h-16 p-0 rounded-full flex items-center justify-center"
+								class="border-black border min-w-[60px] min-h-[60px] rounded-full flex items-center justify-center"
 							>
 								<h4>2</h4>
 							</div>
-							<p class="text-lg whitespace-normal w-40 hover:font-medium pb-8">
-								Install and start to explore.
-							</p>
+							<p class="text-lg hover:font-medium w-full lg:w-40">Install and start to explore.</p>
 						</div>
 					</div>
 					<div
