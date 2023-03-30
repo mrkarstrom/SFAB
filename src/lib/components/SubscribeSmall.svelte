ed75124c4ec383a75dcd5e8a260c7f1aa34db85f
Magnus Karström <magnus@yesweb.se>
2023-03-30 09:04:26 +0200

    fix (SubscribeSmall.svelte): hr-line and footer icons

--- src/lib/components/SubscribeSmall.svelte
+++ src/lib/components/SubscribeSmall.svelte
@@ -3,12 +3,21 @@
 	class="flex flex-col w-full justify-center items-center h-full border-2 px-0 py-0
 			lg:hidden"
 >
-	<img
-		src="https://yesweb.se/sfab/img/mockup/download-app-footer.svg"
-		class="w-[var(--bottomBannerHeight)] rotate-90 absolute -translate-y-[180px]"
-		alt="Download App"
-	/>
-	<div id="newsletter-signup" class="pt-20 w-full px-10 mt-[var(--bottomBannerHeight)]">
+	<figure class="card w-[90%] items-center justify-evenly bg-secondary h-[500px] drop-shadow-md">
+		<div class="flex flex-col  items-center jsutify-center">
+			<p class="text-xl p-4">Countdown to launch</p>
+			<svg
+				fill="#000000"
+				width="60px"
+				height="60px"
+				viewBox="0 0 20 20"
+				xmlns="http://www.w3.org/2000/svg"><path d="M5 6h10l-5 9-5-9z" /></svg
+			>
+		</div>
+		<p class="font-['Steelfish'] text-[100px] uppercase">Slowfashion</p>
+	</figure>
+
+	<div id="newsletter-signup" class="w-full px-10 pt-16">
 		<h2 class="pb-6 mt-8">NEWSLETTER</h2>
 		<form action="" class="flex flex-row items-center">
 			<input
@@ -21,17 +30,19 @@
 			<button class="bg-accent px-6 w-64 h-20 text-3xl rounded-lg m-0">SIGN UP</button>
 		</form>
 	</div>
-	<h5 class="pb-4 text-2xl w-full px-10 mt-6">Join our community and be part of the exciting launch</h5>
-		
+	<h5 class="pb-4 text-2xl font-[500] w-full px-10 mt-6 mb-14">
+		Join our community and be part of the exciting launch
+	</h5>
+	<hr class="border-t-black border-t-1 border-solid w-[550px] h-20 mt-6" />
+
 	<div id="links" class="flex items-center space-x-4 max-w-fit flex-1">
 		<a
 			href="#"
 			id="linked-in-link"
-			class="animate-pulse one rounded-full flex items-center justify-center bg-secondary h-16 w-16"
+			class="animate-pulse one rounded-full flex items-center justify-center bg-secondary h-28 w-28"
 		>
 			<svg
-				width="24px"
-				height="24px"
+				width="50px"
 				viewBox="0 0 24 24"
 				xmlns="http://www.w3.org/2000/svg"
 				fill="none"
@@ -45,25 +56,26 @@
 				/><rect x="2" y="9" width="4" height="12" /><circle cx="4" cy="4" r="2" /></svg
 			>
 		</a>
+
 		<a
 			href="#"
-			id="twitter-link"
-			class="animate-pulse two rounded-full flex items-center justify-center bg-secondary h-16 w-16"
+			id="instagram-link"
+			class="animate-pulse three rounded-full flex items-center justify-center bg-secondary h-28 w-28"
 		>
-			<svg xmlns="http://www.w3.org/2000/svg" fill="black" width="20px" viewBox="0 0 512 512">
+			<svg xmlns="http://www.w3.org/2000/svg" fill="black" width="45px" viewBox="0 0 448 512">
 				<path
-					d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"
+					d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"
 				/></svg
 			>
 		</a>
 		<a
 			href="#"
-			id="instagram-link"
-			class="animate-pulse three rounded-full flex items-center justify-center bg-secondary h-16 w-16"
+			id="twitter-link"
+			class="animate-pulse two rounded-full flex items-center justify-center bg-secondary h-28 w-28"
 		>
-			<svg xmlns="http://www.w3.org/2000/svg" fill="black" width="20px" viewBox="0 0 448 512">
+			<svg xmlns="http://www.w3.org/2000/svg" fill="black" width="50px" viewBox="0 0 512 512">
 				<path
-					d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"
+					d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"
 				/></svg
 			>
 		</a>
