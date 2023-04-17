b4a68d80e36cf12845387da486a42d5c2e89ba61
Magnus Karström <magnus@yesweb.se>
2023-04-17 12:16:46 +0200

    fix (tac): Adding mor text to the tac.

--- src/routes/terms_and_conditions/+page.svelte
+++ src/routes/terms_and_conditions/+page.svelte
@@ -415,6 +415,215 @@
 				</li>
 			</ul>
 		</h6>
+		<h5 class="pl-6 py-2 font-bold">
+			5.3. Use of Electronic Money to Carry Out a Payment Operation
+		</h5>
+		<h6>
+			Before issuing an Order, the User must ensure that they have a sufficient amount of Electronic
+			Money available to cover the amount of the Payment Operation and the related fees as
+			established in the Price Conditions. <br />
+			If necessary, they must acquire sufficient Electronic Money in compliance with Article 5.1 before
+			the Order may be validly transferred to the Issuer to carry it out. Electronic Money may only be
+			held by the User subject to the effective issuance of corresponding funds. Electronic Money in
+			no way may be issued for credit. Additionally, when the amount of Electronic Money available on
+			the date the Order is carried out by the Issuer is below the amount of the Payment Operation (including
+			fees), the Order will automatically be refused by the Issuer. The information about this refusal
+			will be made available to the User on the Site. This Refusal may give rise to additional fees according
+			to the Price Conditions.<br />
+			The methods for transmitting an Order by the User are the following:<br />
+			When the User wishes to carry out a Payment Operation, they will indicate their identification
+			in their Personal Online Area by indicating their Identification Data and, if need be, by following
+			a Strong Authentication procedure if indicated to them. They will fill out the proper form on the
+			Payment Page and, if need be, indicate the proof documents requested by the Issuer. The form must
+			indicate the following elements: the amount in euros (€) of the Payment Operation, the elements
+			allowing for identification of the Beneficiary, the date the Order is executed and any other required
+			information. <br />
+			The User irrevocably consents to the Payment Order by clicking on the “validation” button (“Date
+			of Receipt”). The receipt of the Payment Order is confirmed in the User’s Personal Online Area
+			or by email. No Order may be withdrawn by the User after the date upon which it is deemed to irrevocably
+			have been received, which is from the Date of Receipt. <br />
+			In certain cases, the User may fill out a single form containing an order to transfer funds such
+			as established in Article 5.1 and an Order allowing Payment Operations to be carried out in line
+			with the preceding paragraph.<br />
+			Carrying Out the Order:<br />
+			The amount of the Payment Operation will be deducted from the Electronic Money available in the
+			Account to be credited in favour of the Beneficiary following the User’s instructions. Additionally,
+			the Beneficiary may open an Account pursuant to Article 4.2 to receive the Electronic Money if
+			they are not already a User. The funds corresponding to the Electronic Money used, if it be the
+			case, may be directly transferred to a bank account or payment account open in the name of the
+			Beneficiary upon receiving the information of this account by the Issuer. For this purpose, the
+			Beneficiary must provide the IBAN number and the SWIFT code of their bank account or payment account
+			as well as the address. This account must be open in a banking or payment institution established
+			in a Member State of the European Union or a State that is part of the European Economic Area agreement
+			or a third-party country imposing equivalent obligations in terms of the fight against money laundering
+			and the financing of terrorism.<br />
+			It is expressly agreed that the Orders are executed at the latest at the end of the Business Day
+			following the Date of Receipt of the Order by the Issuer (and on the agreed-to execution date for
+			standing or timely transfers) if the Beneficiary has an Account. If necessary, the Date of Receipt
+			may be delayed until an Account is opened or until the day the Issuer has gathered the information
+			of the bank account or payment account of the Beneficiary receiving the funds.<br />
+			Any Payment Order received after 4:00 p.m. by the Service Provider will be deemed to have been
+			received the following Business Day. If the Date of Receipt is not a Business Day, the Order will
+			be deemed to have been received the following Business Day.
+		</h6>
+		<h5 class="pl-6 py-2 font-bold">5.4. Transmission and Execution of a Reimbursement Order</h5>
+		<h6>
+			When a Reimbursement Order is requested, the User will be identified on the Site by indicating
+			their Identification Data or connecting via their Facebook account. They will fill out the
+			proper form on the Payment Page and, if need be, indicate the proof documents requested by the
+			Issuer. The User must indicate on the form the following elements: the amount of the
+			Reimbursement, the date the Order is to be executed and any other required information.<br />
+			The User irrevocably consents to the Payment Order by clicking on the “validation” button (“Date
+			of Receipt”). The receipt of the Payment Order is confirmed in the User’s Personal Online Area
+			or by email. No Order may be withdrawn by the User after the date upon which it is deemed to irrevocably
+			have been received, which is from the Date of Receipt. <br />
+			Reimbursement will be carried out by crediting the Card (or Payment Method) that the User used
+			to acquire the Electronic Money. If it be the case, it will be carried out by funds transfer to
+			the bank account or payment account of the Beneficiary the information of which has been provided
+			to the Issuer (“Notification Date”). It is agreed to between the Parties that the Reimbursement
+			will be carried out at the latest at the end of the Business Day following the receipt or notification
+			depending on the case.<br />
+			Any Reimbursement Order received after 4:00 p.m. by the Service Provider will be deemed to have
+			been received the following Business Day. If the Date of Receipt is not a Business Day, the Order
+			will be deemed to have been received the following Business Day.
+		</h6>
+		<h5 class="pl-6 py-2 font-bold">5.5. Withdrawing an Order</h5>
+		<h6>
+			No Order may be withdrawn by the User after the date upon which it is deemed to irrevocably
+			have been received as indicated above.
+		</h6>
+		<h5 class="pl-6 py-2 font-bold">6. Reporting</h5>
+		<h6>
+			The User, in their Personal Online Area, has a statement of the Operations carried out
+			available to them. They are asked to attentively acknowledge the list of these operations. The
+			User recognises that only the statement of the Payment Operation validated by the Issuer is
+			deemed as recognition by the latter of the Electronic Money available in the Account on the
+			day indicated on the statement. The User may, at any time on their personal page accessible on
+			the Site, access an indication of the amount of Electronic Money available in the Account. <br
+			/>
+			It is specified that for each Operation carried out on the Account, the User has the following
+			information available to them: the reference number of the Operation, the identification of the
+			Beneficiary, the amount of the Operation, the date the Order is received, and if it be the case,
+			the fees related to executing this Operation. <br />
+			The Issuer will provide the User upon request the monthly statements of the Account for the previous
+			thirteen (13) months.
+		</h6>
+		<h5 class="pl-6 py-2 font-bold">7. Objection Regarding Personalised Security Data</h5>
+		<h6>
+			The User must inform the Distributor of the loss or theft of their Personalised Security Data,
+			of any misuse or unauthorised use of their Personal Online Area or their data as soon as they
+			become aware of this and request that it be blocked. This declaration must be carried out:
+			<ul id="the-amount-of-electronic-money" class="list-['-'] ml-10 pt-2">
+				<li class="pl-4">
+					by making a telephone call to the Distributor Customer Service at the number indicated in
+					the General Conditions of the Site; or
+				</li>
+				<li class="pl-4">
+					directly by electronic message through the contact form accessible on the Site.
+				</li>
+			</ul>
+			The Issuer, through the Distributor, shall immediately execute the request for blocking the identifier
+			in question. The event will be recorded and date/time stamped. An objection number with date/time
+			stamp will be provided to the User. Written confirmation of this objection will be sent by the
+			Distributor to the User in question by electronic message. The Issuer will take administrative
+			responsibility of the file and keep all proof relating to it for 18 (eighteen) months. Upon written
+			request of the User and before this time frame expires, the Issuer will provide a copy of this
+			objection.<br />
+			Any request for objection must be confirmed immediately by the User in question, by a letter signed
+			by the latter, provided or sent by registered mail, or email, to the Issuer at the postal address
+			indicated hereinabove. The Issuer and the Distributor will not be held responsible for the consequences
+			of an objection sent by fax or email that does not come from the User.<br />
+			A request for objection is deemed to be made on the date and time of the effective receipt of the
+			request by the Distributor. In the event of theft or fraudulent use of the Identifier, the Issuer
+			is authorised to request from the Distributor, a statement or copy of the complaint of the User
+			and undertakes to respond to it as quickly as possible.
+		</h6>
+		<h5 class="pl-6 py-2 font-bold">8. Contesting an Operation</h5>
+		<h5 class="pl-6 pb-2 font-bold">8.1. Provisions Common to All Users</h5>
+		<h6>
+			For any claim relating to Payment or Reimbursement Operations carried out by the Issuer in the
+			framework hereof, the User is asked to address the Distributor Customer Service at the address
+			indicated for this purpose in the General Conditions of the Site. <br />
+			If an Order is executed by the Issuer with errors attributed to this latter, this should be contested
+			as soon as possible to the Service Provider, the Order will then be cancelled and the Account returned
+			to the situation that it was in before receiving the Payment Order. Following that, the Order will
+			be correctly reissued.<br />
+			The fees indicated in the Price Conditions may be due in the event an Operation is unjustifiably
+			contested.
+		</h6>
+		<h5 class="pl-6 py-2 font-bold">8.2. Provisions Applicable to the Professional User</h5>
+		<h6>
+			The Professional User wishing to contest a Transfer Operation unauthorised by them or
+			improperly executed must contact the Distributor Customer Service by telephone as soon as
+			possible after they become aware of the anomaly and at the latest within eight (8) weeks
+			following the transaction of the operation, it being their responsibility to contest it to the
+			Issuer as soon as possible. Unless there are good reasons to suspect the User of fraud, the
+			Issuer will reimburse the User in the amount of the Operation immediately following receiving
+			the request to contest it, and in any case at the latest at the end of the next Business Day.
+			The Issuer will return the Account to the state it was in before the unauthorised Payment
+			Operation took place <br />
+			In the event of the loss or theft of Personalised Security Data, unauthorised Operations carried
+			out before they are contested are the User’s responsibility. Operations carried out after they
+			are contested are borne by the Issuer unless in the event of fraud by the User.
+		</h6>
+		<h5 class="pl-6 py-2 font-bold">8.3. Provisions Applicable to the Consumer User</h5>
+		<h6>
+			The Consumer User wishing to contest a Transfer Operation unauthorised by them or improperly
+			executed must contact the Distributor Customer Service by telephone as soon as possible after
+			they become aware of the anomaly and at the latest within thirteen (13) months following the
+			date it is debited, it being their responsibility to contest it to the Issuer as soon as
+			possible. Unless there are good reasons to suspect the User of fraud, the Issuer will
+			reimburse the User in the amount of the Operation immediately following receiving the request
+			to contest it, and in any case at the latest at the end of the next Business Day. The Issuer
+			will return the Account to the state it was in before the unauthorised Payment Operation took
+			place<br />
+			In the event it is contested, responsibility for proof that the Operation was identified, duly
+			recorded and accounted for, and that it was not affected by technical or other deficiencies is
+			the responsibility of the Issuer.<br />
+			In the event of an unauthorised Payment Operation following the loss or theft of Personalised Security
+			Data, the User is responsible for the losses related to the use of Personalised Security Data before
+			it is contested, up to a threshold of fifty (50) euros. Operations carried out after they are contested
+			are borne by the Issuer unless in the event of fraud by the User. However, the User is not held
+			responsible in the event:
+			<ul id="the-amount-of-electronic-money" class="list-['-'] ml-10 py-2">
+				<li class="pl-4">
+					Of an unauthorised Payment Operation carried out without using Personalised Security Data;
+				</li>
+				<li class="pl-4">
+					Of the loss or theft of Personalised Security Data that could not be detected by the User
+					before the payment was made;
+				</li>
+				<li class="pl-4">
+					Of losses due to actions or failures of an employee, agent or subsidiary of a PSP or an
+					entity to which these activities were externalised.
+				</li>
+			</ul>
+			The User is also not held responsible:
+			<ul id="the-amount-of-electronic-money" class="list-['-'] ml-10 pt-2">
+				<li class="pl-4">
+					if the unauthorised Payment Operation is carried out by diverting the Personalised
+					Security Data, without the User’s knowledge;
+				</li>
+				<li class="pl-4">
+					in the event of counterfeiting the Personalised Security Data, if, at the time of the
+					unauthorised Payment Operation, the User is in possession of this Data.
+				</li>
+			</ul>
+			The User will bear all the losses arising from unauthorised Operations if these losses result from
+			fraudulent activity by them or if they intentionally seriously neglected the obligations to keep
+			their Personalised Security Data secured and to contest operations in the event of loss, theft
+			or diversion of their Data.<br />
+			Barring fraudulent activities on behalf of the User, the latter will not bear any financial consequences
+			if the unauthorised Operation was carried out without the Issuer requiring Strong Authentication
+			of the User, in the event that regulations require it.
+			<h5 class="pl-6 py-2 font-bold">9. Financial Conditions</h5>
+			The services offered herein are invoiced by the Distributor in the name and on behalf of the Issuer
+			in compliance with the Price Conditions.<br /> Any commissions due by the User are automatically
+			deducted from the Electronic Money Account by the Issuer. The User authorises the Issuer to compensate
+			at any time, even after the Account is closed, any irrefutable credit, liquid and collectible that
+			remains owed, of any nature whatsoever. Funds in the Electronic Money Account may be compensated
+			for any amount due, collectible and unpaid of the User to the Issuer.
+		</h6>
 	</section>
 </section>
 <Footer />
