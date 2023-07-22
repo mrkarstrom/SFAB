2285d6f7acc2a23a8ecc06be047fc0f35743941b
Magnus Karström <magnus@yesweb.se>
2023-07-22 22:32:29 +0200

    feat (CarePage.svelte): Added the carepage with the GridBlock.svelte

--- src/routes/carepage/+page.svelte
+++ src/routes/carepage/+page.svelte
@@ -1,5 +1,16 @@
 <script>
-	import CarePage from "../../lib/components/carepage/CarePage.svelte";
+	import ArrowBlock from '$lib/components/arrows/ArrowBlock.svelte';
+	import CareHero from '../../lib/components/carepage/CareHero.svelte';
+	import GridSection1 from '../../lib/components/carepage/GridSection1.svelte';
+	import GridSection2 from '../../lib/components/carepage/GridSection2.svelte';
+	import GridSection3 from '../../lib/components/carepage/GridSection3.svelte';
 </script>
 
-<CarePage />
+<main class="flex flex-col justify-center px-6 gap-4">
+	<CareHero />
+	<GridSection1 />
+	<ArrowBlock />
+	<GridSection2 />
+	<ArrowBlock />
+	<GridSection3 />
+</main>
